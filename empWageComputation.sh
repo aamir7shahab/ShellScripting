498f9adf9151e20b0d0221cb46a7bf7ae6ff300a
aamir7shahab <aamir.shahab007@gmail.com>
2020-07-08 21:30:37 +0530

    monthly wage code added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,6 +6,8 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 WAGE_PER_HOUR=20
 EMPLOYEE_HOUR_FULLTIME=8
 EMPLOYEE_HOUR_PARTTIME=4
+WORKING_DAY_PER_MONTH=20
+
 
 # Check Employee is Present or Absent
 attendance=$((RANDOM%2))
@@ -30,7 +32,6 @@ partTimeWage=$((WAGE_PER_HOUR*EMPLOYEE_HOUR_PARTTIME))
 echo "Part Time Wage : $partTimeWage"
 # >>>>>>> uc3
 # =======
-
 # calculating Employee wage using switch case
 empStatus=$((RANDOM%2+1))
 
@@ -46,3 +47,23 @@ empWage=$((empHrs*WAGE_PER_HOUR))
 echo "Total wage using CASE : $empWage"
 # >>>>>>> Stashed changes
 # >>>>>>> uc4
+
+#claculating Wage for Month
+
+TotalMonthWage=0
+
+for (( i = 0; i < $WORKING_DAY_PER_MONTH; i++ )); do
+	
+	empStatus=$((RANDOM%2+1))
+	case $empStatus in
+		1 ) empHrs=$EMPLOYEE_HOUR_PARTTIME
+			;;
+		2 ) empHrs=$EMPLOYEE_HOUR_FULLTIME
+			;;
+		* ) empHrs=0
+			;;
+	esac
+	empWage=$((empHrs*WAGE_PER_HOUR))
+	TotalMonthWage=$(( TotalMonthWage + empWage ))
+done
+echo "Monthly Wage : $TotalMonthWage"
