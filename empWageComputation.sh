06abf223cb09250b097d0e3f37867c39add10d84
aamir7shahab <aamir.shahab007@gmail.com>
2020-07-08 20:50:06 +0530

    Resolved merge conflict

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,8 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
+<<<<<<< Updated upstream
 # <<<<<<< HEAD
+#CONSTANT
 #CONSTANT
 WAGE_PER_HOUR=20
 EMPLOYEE_HOUR_FULLTIME=8
@@ -14,15 +16,36 @@ if [[ $attendance -eq 0 ]]; then
 else
 	echo "Employee is Present"
 fi
+
 # =======
 # calculating daily Employee Wage
 
 dailyWage=$(( WAGE_PER_HOUR * EMPLOYEE_HOUR_FULLTIME ))
 echo "Dialy Wage : $dailyWage"
 # >>>>>>> uc2
+
 # =======
 # Add Part time Employee & Wage
 
 partTimeWage=$((WAGE_PER_HOUR*EMPLOYEE_HOUR_PARTTIME))
 echo "Part Time Wage : $partTimeWage"
 # >>>>>>> uc3
+
+# =======
+# calculating Employee wage using switch case
+empStatus=$((RANDOM%2+1))
+
+case $empStatus in
+	1 ) empHrs=$EMPLOYEE_HOUR_PARTTIME
+		break
+		;;
+	2 ) empHrs=$EMPLOYEE_HOUR_FULLTIME
+		break
+		;;
+	* ) empHrs=0
+		break
+		;;
+esac
+empWage=$((empHrs*WAGE_PER_HOUR))
+echo "Total wage using case : $empWage"
+>>>>>>> Stashed changes
