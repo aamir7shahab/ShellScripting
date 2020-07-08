df7c31842fba6e6ea04381b561c912ee9c92ff8b
aamir7shahab <aamir.shahab007@gmail.com>
2020-07-08 20:17:47 +0530

    calculating part-time Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,8 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+# Add Part time Employee & Wage
+WAGE_PER_HOUR=20
+EMPLOYEE_HOUR_PARTTIME=4
+
+partTimeWage=$((WAGE_PER_HOUR*EMPLOYEE_HOUR_PARTTIME))
+echo "Part Time Wage : $partTimeWage"
