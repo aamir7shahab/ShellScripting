cc7e577434a837aba13269749c7b9796eb0c9c81
aamir7shahab <aamir.shahab007@gmail.com>
2020-07-08 20:12:13 +0530

    calculating daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,9 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+# calculating daily Employee Wage
+#CONSTANT
+WAGE_PER_HOUR=20
+EMPLOYEE_HOUR_FULLTIME=8
+
+dailyWage=$(( WAGE_PER_HOUR * EMPLOYEE_HOUR_FULLTIME ))
+echo "Dialy Wage : $dailyWage"
