823f9a0b308ac545b38b57c4c3aff038afcbbe4f
aamir7shahab <aamir.shahab007@gmail.com>
2020-07-08 20:02:48 +0530

    added code to check present or absent using branch uc1

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,7 +2,6 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 
 # Check Employee is Present or Absent
 attendance=$((RANDOM%2))
-echo $attendance
 
 if [[ $attendance -eq 0 ]]; then
 	echo "Employee is Absent"
